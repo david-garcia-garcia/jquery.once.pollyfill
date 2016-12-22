ce9a551cb9b9e49a820af4aa4893598eb9a70aa6
David García García <deivid.garcia.garcia@gmail.com>
2016-12-22 12:12:08 +0100

    Initial commit.

---
+++ jquery.once.pollyfill.js
@@ -0,0 +1,15 @@
+﻿(function () {
+
+	var $original_once = jQuery.fn.once;
+
+	jQuery.fn.once = function ($id, $each) {
+		if (typeof ($id) == 'function') {
+			return $original_once.apply(this, ['once-pollyfill']).each($id);
+		}
+		if (typeof ($id) == 'string' && typeof ($each) == 'function') {
+			return $original_once.apply(this, [$id]).each($each);
+		}
+		return $original_once.apply(this, [$id]);
+	};
+
+})();
